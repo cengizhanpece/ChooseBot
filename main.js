d2aa1674f4a5317de267393378600f2181d64738
Cengizhan <cengizhanpe@gmail.com>
2019-11-03 20:23:54 +0300

    add !sec help status

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    client.User.setGame("!sec help");
 });
 
 client.on('message', msg => {
