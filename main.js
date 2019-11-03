8a5cd51fa8f56a7828ec1f84aed470fc0adcfb3a
Cengizhan <cengizhanpe@gmail.com>
2019-11-03 20:28:57 +0300

    ll

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity(`this won't appear in the bot's custom status!`, { type: 4 });
+    client.user.setGame("!sec help");
 });
 
 client.on('message', msg => {
