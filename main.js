bc7f5d3b58fee8070ca098a4ea9d1ca7fd7ab082
Cengizhan <cengizhanpe@gmail.com>
2019-11-04 17:38:55 +0300

    Add command line description and delete discord token from code(added env)

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+/*
+
+# This bot created for deciding any kind stuff
+# If you cant choose one thing you can let the bot choose for you
+# Example usage !sec [element1] [element2] 
+# Elements must split by space
+# Required at least 2 element
+
+*/
+
+// import discord.js
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -6,21 +17,32 @@ client.on('ready', () => {
     client.user.setGame("!sec help");
 });
 
+// This function called when someone send message
 client.on('message', msg => {
     if (msg.content.startsWith('!sec')) {
         let array = msg.content.split(" ");
+        //only works for !sec not something like !secaaa
+        if(array[0] != "!sec"){
+            return;
+        }
+        // if message content only !sec help reply how to use bot
         if(msg.content == "!sec help"){
             msg.reply("\`\`\` Örnek Kullanım: !sec [seçenek1] [seneçek2] \n (Seçenekleri boşluk ile ayırın).\`\`\`");
             return;
         }
+        //delete !sec command from array (its here because we split the message)
         array.shift();
+        //if array has only one element reply with error message and how to use bot
         if(array.length < 2){
             msg.reply("\`\`\` En az iki seçenek giriniz. \n Örn: !sec [seçenek1] [seneçek2]\`\`\`");
             return;
         }
+        // get a random number 0 to array length
         let randomNumber = Math.floor(Math.random() * array.length);
+        //reply a array index with choosen by random
         msg.reply("Kaderinde " + array[randomNumber] + " varmış");
     }
 });
 
-client.login('NjQwNTg0Nzc5MzgwNTU1ODA0.Xb7-YA._VF5eabibg5xCM_Grh2YeTd9C1g');
+//Discord token
+client.login(process.env.TOKEN);
